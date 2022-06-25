2f567ede28f2b58e19f7d6bc272eef64754faee5
Dayane Sallet <dayanesallet@hotmail.com>
2022-06-25 11:54:02 -0300

    Alteração na importação da imagem da pagina de inscrição

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import htmlImages from 'vite-plugin-html-images'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()]
 })
+
